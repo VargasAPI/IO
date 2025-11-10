bf31ba7ca6d50d11ff490b0a0f4ee6d051c318d4
tim-scarlith <anthony00052023@gmail.com>
2025-11-10 12:11:36 -0600

    archivos de algoritmo arboles binarios

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from "./pages/Main.jsx";
 // import ReemplazoEquipos from './pages/ReemplazoEquipos.jsx';
-// import ArbolesBinarios from './pages/ArbolesBinarios.jsx';
+ import ArbolesBinarios from './pages/ArbolesBinarios.jsx';
 // import SeriesDeportivas from './pages/SeriesDeportivas.jsx';
  import MultiplicacionMatrices from './pages/MultiplicacionMatrices.jsx';
 
 function App() {
-  return (
+return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Main />} />
-          {/* <Route path="/reemplazo-equipos" element={<ReemplazoEquipos />} />
+          {/* <Route path="/reemplazo-equipos" element={<ReemplazoEquipos />} /> */}
           <Route path="/arboles-binarios" element={<ArbolesBinarios />} />
-          <Route path="/series-deportivas" element={<SeriesDeportivas />} />*/
-          <Route path="/multiplicacion-matrices" element={<MultiplicacionMatrices />} />} 
+          {/* <Route path="/series-deportivas" element={<SeriesDeportivas />} /> */}
+          <Route path="/multiplicacion-matrices" element={<MultiplicacionMatrices />} /> 
         </Routes>
       </div>
     </Router>
